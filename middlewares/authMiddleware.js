ca104613c41d7c4ff8e8433378fbd918db1fb675
Artem Grankin <artem_grankin@epam.com>
2020-10-13 20:25:32 +0300

    feature: add auth middleware

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken')
+
+const authMiddleware = (req, res, next) => {
+  if (req.method === 'OPTIONS') {
+    return next()
+  }
+
+  const resNoAuthMessage = () =>
+    res.status(401).json({ message: 'No authorization' })
+
+  try {
+    const token = req.headers.authorization.split(' ')[1]
+
+    if (!token) resNoAuthMessage()
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = decoded
+    next()
+  } catch (e) {
+    resNoAuthMessage()
+  }
+}
+
+module.exports = authMiddleware
