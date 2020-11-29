be80d85fd93d0fb6c15de156543f3d23c852c588
Artem Grankin <artem_grankin@epam.com>
2020-11-29 15:45:47 +0300

    feature: save user data in ls

---
+++ client/src/local-storage-controller/localStorageController.js
@@ -0,0 +1,19 @@
+const storage = window.localStorage
+
+const localStorageController = {
+  getUser() {
+    return {
+      token: JSON.parse(storage.getItem('token')),
+      email: JSON.parse(storage.getItem('email')),
+      userId: JSON.parse(storage.getItem('userId')),
+    }
+  },
+
+  setUser({ token, userId, email }) {
+    storage.setItem('token', JSON.stringify(token))
+    storage.setItem('userId', JSON.stringify(userId))
+    storage.setItem('email', JSON.stringify(email))
+  },
+}
+
+export default localStorageController
--- client/src/thunk-functions/userThunkFunctions.js
+++ client/src/thunk-functions/userThunkFunctions.js
@@ -1,4 +1,5 @@
 import { loginUser } from '../actions/userActions'
+import localStorageController from '../local-storage-controller/localStorageController'
 
 const loginUserThunk = (data) => async (dispatch, getState) => {
   try {
@@ -13,8 +14,9 @@ const loginUserThunk = (data) => async (dispatch, getState) => {
     if (!response.ok) {
       throw new Error(data.message || 'Something went wrong')
     }
-
+    const { email } = data
     const { token, userId } = await response.json()
+    localStorageController.setUser({ token, userId, email })
     dispatch(loginUser(token, data.email, userId))
   } catch ({ message }) {
     alert(message)
